a6da45d2cecf81b30479de05bdd48b7655c32486
sinclairstudy <sinclairstudy@gmail.com>
2018-04-02 06:22:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching (names, string) {
+  return names.filter(name => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch (names, string) {
+  return names.filter(name => name.slice(0, string.length) === string);
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(driver => driver['name'] === string);
+}
