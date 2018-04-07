11cd23669a8b7b3b57ec656feffc187a7f14d990
bafab <bafa@tutanota.de>
2018-04-07 16:50:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 function findMatching(drivers, string) {
   return drivers.filter(function (name) {
-    return name.toLowerCase() === string.toLowerCase();
-  });
+    return name.toLowerCase() === string.toLowerCase()
+  })
 }
-
 function fuzzyMatch(drivers, string) {
-  let length = string.length;
   return drivers.filter(function (name) {
-    return name.slice(0, length) === string;
-  });
+    return name.slice(0, string.length) === string
+  })
 }
-
-function matchName(drivers, name) {
-  return drivers.filter(function (driver) {
-    return driver.name.toLowerCase() === name.toLowerCase();
-  });
+function matchName(drivers, string) {
+  return drivers.filter(function (x) {
+    return x.name === string
+  })
 }
